ec4dc6ea7ca7eb43680d1cc9d32bd2e9af3ddcd0
James Whitney <jamesdrums@mac.com>
2018-02-19 13:57:26 -0500

    feat: Adds more logic to ViewCell
    Last function in the VillainCollectionViewController will segue
    to VillianDetailViewController.

--- BondVillains/VillainCollectionViewController.swift
+++ BondVillains/VillainCollectionViewController.swift
@@ -28,4 +28,14 @@ class VillainCollectionViewController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.villainsCollection.count
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "VillainCollectionViewCell", for: indexPath) as! VillainCollectionViewCell
+        let villain = self.villainsCollection[(indexPath as NSIndexPath).row]
+        
+        cell.villainName.text = villain.name
+        cell.villainImageView?.image = UIImage(named: villain.imageName)
+        
+        return cell
+    }
 }
