43be41b4cecfa6ae7289c734396301aa6ea26ddf
James Whitney <jamesdrums@mac.com>
2018-02-19 13:37:35 -0500

    feat: VillainCollectionViewCell adds outlets
    Created class to hold properties for the VillainCollectionViewCell.

---
+++ BondVillains/VillainCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  VillainCollectionViewCell.swift
+//  BondVillains
+//
+//  Created by James Whitney on 2/19/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class VillainCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var villainImageView: UIImageView!
+    @IBOutlet weak var villainName: UILabel!
+    
+}
